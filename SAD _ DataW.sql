a8206b35d536298dd890da9166e44c1b37ed4ffd
Jairo Rios <jairo.rios@hotmail.com>
2024-04-16 16:39:41 -0300

    Pronto

--- SAD _ DataW.sql
+++ SAD _ DataW.sql
@@ -1,3 +1,60 @@
+-- Criando o banco de dados
+CREATE DATABASE "Atividade_SAD";
+
+-- Selecionando o banco
+USE "Atividade_SAD";
+
+-- Criando tabela (Já na atividade)
+CREATE TABLE Vendas (
+    VendaID INT IDENTITY(1,1) PRIMARY KEY,
+    DataVenda DATETIME,
+    ValorVenda DECIMAL(10, 2),
+    ClienteID INT,
+    NomeCliente NVARCHAR(100),
+    EnderecoCliente NVARCHAR(255),
+    ProdutoID INT,
+    NomeProduto NVARCHAR(100),
+    PrecoUnitario DECIMAL(10, 2),
+    Quantidade INT,
+    VendedorID INT,
+    NomeVendedor NVARCHAR(100)
+);
+
+-- Inserindo Dados (Já na atividade)
+DECLARE @i INT = 0;
+
+WHILE @i < 200
+BEGIN
+    INSERT INTO Vendas (
+        DataVenda,
+        ValorVenda,
+        ClienteID,
+        NomeCliente,
+        EnderecoCliente,
+        ProdutoID,
+        NomeProduto,
+        PrecoUnitario,
+        Quantidade,
+        VendedorID,
+        NomeVendedor
+    )
+    VALUES (
+        GETDATE(),
+        ROUND((RAND() * (500 - 50) + 50), 2),
+        @i % 10 + 1,
+        'Cliente ' + CAST((@i % 10 + 1) AS NVARCHAR(10)),
+        'Endereço ' + CAST((@i % 10 + 1) AS NVARCHAR(10)),
+        @i % 5 + 1,
+        'Produto ' + CAST((@i % 5 + 1) AS NVARCHAR(10)),
+        ROUND((RAND() * (100 - 10) + 10), 2),
+        @i % 3 + 1,
+        @i % 4 + 1,
+        'Vendedor ' + CAST((@i % 4 + 1) AS NVARCHAR(10))
+    );
+
+    SET @i = @i + 1;
+END
+
 -- Tabela Dimensão de Data
 CREATE TABLE DimensaoData (
     DataID INT IDENTITY(1,1) PRIMARY KEY,
